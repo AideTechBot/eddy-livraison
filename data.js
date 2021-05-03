82658a068c9e6589fcb720cc8440c71b5104cda3
aidetechbot <mandio0302@gmail.com>
2021-05-03 00:41:37 -0400

    Added more data

--- data.js
+++ data.js
@@ -18,6 +18,24 @@ export const RESTAURANT_DATA = [
         ]
     },
     {
+        name: "Pür & Simple",
+        address: "30 Rue de l'Église",
+        dineIn: true,
+        takeOut: true,
+        delivery: true,
+        orderURL: "https://pur-simple.order-online.ai/en-CA/#/",
+        phoneNumber: "15067337310",
+        openHours: [
+            [7,15], //starts on sunday
+            [6,15],
+            [6,15],
+            [6,15],
+            [6,15],
+            [6,15],
+            [7,15],
+        ]
+    },
+    {
         name: "Greco Pizza",
         address: "56 Chemin Canada",
         dineIn: true,
@@ -108,6 +126,24 @@ export const RESTAURANT_DATA = [
         ]
     },
     {
+        name: "The Pirate",
+        address: "1791 Rue Principale",
+        dineIn: true,
+        takeOut: true,
+        delivery: true,
+        orderURL: "",
+        phoneNumber: "15067357111",
+        openHours: [
+            [11,20.00], //starts on sunday
+            [11,20.00],
+            [11,20.00],
+            [11,20.00],
+            [11,20.00],
+            [11,20.00],
+            [11,20.00],
+        ]
+    },
+    {
         name: "KFC",
         address: "180 Boulevard Hébert",
         dineIn: true,
@@ -126,21 +162,255 @@ export const RESTAURANT_DATA = [
         ]
     },
     {
-        name: "The Pirate",
-        address: "1791-A Main Street (M.M.F.N.)",
+        name: "Dejeuner AM Breakfast",
+        address: "1791-B Rue Principale M.M.F.N.",
         dineIn: true,
         takeOut: true,
-        delivery: true,
+        delivery: false,
         orderURL: "",
-        phoneNumber: "15067357111",
+        phoneNumber: "15067351313",
         openHours: [
-            [11,22.00], //starts on sunday
-            [11,22.00],
-            [11,22.00],
-            [11,22.00],
-            [11,22.00],
-            [11,22.00],
-            [11,22.00],
+            [7,13.30], //starts on sunday
+            [7,13.30],
+            [7,13.30],
+            [7,13.30],
+            [7,13.30],
+            [7,13.30],
+            [7,13.30],
         ]
+    },
+    {
+        name: "The Valley View Restaurant",
+        address: "100 Chief Joanna Blvd",
+        dineIn: true,
+        takeOut: true,
+        delivery: false,
+        orderURL: "",
+        phoneNumber: "15067353151",
+        openHours: [
+            [16,21], //starts on sunday
+            [16,21],
+            [16,21],
+            [16,22.00],
+            [16,22.00],
+            [16,22.00],
+            [16,22.00],
+        ]
+    },
+    {
+        name: "Ouellet Fish and Family Restaurant",
+        address: "359 Rue Victoria",
+        dineIn: true,
+        takeOut: true,
+        delivery: false,
+        orderURL: "",
+        phoneNumber: "15067395321",
+        openHours: [
+            [6,19.00], //starts on sunday
+            [6,19.00],
+            [6,19.00],
+            [6,19.00],
+            [6,20.00],
+            [6,21.00],
+            [6,19.00],
+        ]
+    },
+    {
+        name: "Cafe Lotus Blue International",
+        address: "52 Chemin Canada",
+        dineIn: true,
+        takeOut: true,
+        delivery: false,
+        orderURL: "",
+        phoneNumber: "15067398259",
+        openHours: [
+            [0,0], //starts on sunday
+            [0,0], 
+            [8,16], 
+            [8,16], 
+            [8,16], 
+            [8,16], 
+            [9,16], 
+       ]
+    },
+    {
+        name: "Sushi World Restaurant",
+        address: "55 Chemin Canada",
+        dineIn: true,
+        takeOut: true,
+        delivery: false,
+        orderURL: "",
+        phoneNumber: "15063531010",
+        openHours: [
+            [15,20], //starts on sunday
+            [0,0], 
+            [0,0], 
+            [0,0], 
+            [15,20], 
+            [15,20], 
+            [15,20], 
+       ]
+    },
+    {
+        name: "Le Deck",
+        address: "192 Rue Victoria",
+        dineIn: true,
+        takeOut: true,
+        delivery: false,
+        orderURL: "",
+        phoneNumber: "15063533325",
+        openHours: [
+            [0,0], //starts on sunday
+            [0,0], 
+            [0,0], 
+            [14,22], 
+            [14,22], 
+            [14,22], 
+            [14,22], 
+       ]
+    },
+    {
+        name: "L'Écluse - Halte Gourmande",
+        address: "116 Rue Victoria",
+        dineIn: false,
+        takeOut: true,
+        delivery: false,
+        orderURL: "http://www.ecluse.ca/",
+        phoneNumber: "15063530521",
+        openHours: [
+            [0,0], //starts on sunday
+            [0,0], 
+            [10,18],
+            [10,18],
+            [10,18], 
+            [10,18], 
+            [0,0], 
+       ]
+    },
+    {
+        name: "JungWon Cuisine",
+        address: "5 Rue Hill",
+        dineIn: false,
+        takeOut: true,
+        delivery: false,
+        orderURL: "",
+        phoneNumber: "15062928012",
+        openHours: [
+            [11,15.00], //starts on sunday
+            [0,0], 
+            [0,0], 
+            [0,0], 
+            [11,20.00], 
+            [11,20.00], 
+            [11,15.00], 
+       ]
+    },
+    {
+        name: "Kinh Do Restaurant Vietnamese",
+        address: "53 Chemin Canada",
+        dineIn: false,
+        takeOut: true,
+        delivery: false,
+        orderURL: "",
+        phoneNumber: "15067397541",
+        openHours: [
+            [16,20.00], //starts on sunday
+            [11,20.00], 
+            [11,20.00], 
+            [11,20.00], 
+            [11,20.00], 
+            [11,20.00], 
+            [11,20.00], 
+       ]
+    },
+    {
+        name: "Tam's Chinese Food",
+        address: "261 Rue Victoria",
+        dineIn: false,
+        takeOut: true,
+        delivery: false,
+        orderURL: "",
+        phoneNumber: "15067353349",
+        openHours: [
+            [11,19.00], //starts on sunday
+            [11,19.00], 
+            [11,19.00], 
+            [11,19.00], 
+            [11,19.00], 
+            [11,19.00], 
+            [11,19.00], 
+       ]
+    },
+    {
+        name: "Frank's Bar & Grill",
+        address: "100 Rue Rice",
+        dineIn: false,
+        takeOut: true,
+        delivery: false,
+        orderURL: "",
+        phoneNumber: "15067398886",
+        openHours: [
+            [7,23.59], //starts on sunday
+            [7,20], 
+            [7,20], 
+            [7,20], 
+            [7,20], 
+            [7,21], 
+            [7,21], 
+       ]
+    },
+    {
+        name: "McDonald's",
+        address: "190 Boulevard Hébert",
+        dineIn: false,
+        takeOut: true,
+        delivery: false,
+        orderURL: "https://www4.mcdonalds.ca/mcdeliverycanada/",
+        phoneNumber: "15067356232",
+        openHours: [
+            [0,24.00], //starts on sunday
+            [0,24.00], 
+            [0,24.00], 
+            [0,24.00], 
+            [0,24.00], 
+            [0,24.00], 
+            [0,24.00], 
+       ]
+    },
+    {
+        name: "Subway",
+        address: "180 Boulevard Hébert",
+        dineIn: true,
+        takeOut: true,
+        delivery: false,
+        orderURL: "https://order.subway.com/en-ca/restaurant/19326-0/menu/?utm_source=yxt-goog&utm_medium=local&utm_term=acq&utm_content=19326&utm_campaign=evergreen-2020&y_source=1_MTQ5MTgzNjgtNzE1LWxvY2F0aW9uLm9yZGVyX3VybA%3D%3D",
+        phoneNumber: "15067366601",
+        openHours: [
+            [9,20.00], //starts on sunday
+            [8,20.00], 
+            [8,20.00], 
+            [8,20.00], 
+            [8,20.00], 
+            [8,20.00], 
+            [8,20.00], 
+       ]
+    },
+    {
+        name: "Tim Hortons",
+        address: "100 Grey Rock Rd.",
+        dineIn: true,
+        takeOut: true,
+        delivery: false,
+        orderURL: "https://order.subway.com/en-ca/restaurant/19326-0/menu/?utm_source=yxt-goog&utm_medium=local&utm_term=acq&utm_content=19326&utm_campaign=evergreen-2020&y_source=1_MTQ5MTgzNjgtNzE1LWxvY2F0aW9uLm9yZGVyX3VybA%3D%3D",
+        phoneNumber: "15067366601",
+        openHours: [
+            [9,20.00], //starts on sunday
+            [8,20.00], 
+            [8,20.00], 
+            [8,20.00], 
+            [8,20.00], 
+            [8,20.00], 
+            [8,20.00], 
+       ]
     },
 ];
